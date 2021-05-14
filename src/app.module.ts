5e2fea88304a1171863b5900222649339dcde8e7
Kyungeun <ruddms936@naver.com>
2021-05-14 10:33:05 +0900

    add: todo-lists endpoint 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TodoListsController } from "./todo-lists/todo-lists.controller";
+import { TodoListsService } from "./todo-lists/todo-lists.service";
+import { TodoListsModule } from "./todo-lists/todo-lists.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodoListsModule],
+  controllers: [AppController, TodoListsController],
+  providers: [AppService, TodoListsService],
 })
 export class AppModule {}
