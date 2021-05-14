b9f55e5abe3f08650da1e01671349d17cd8699d3
Kyungeun <ruddms936@naver.com>
2021-05-14 11:11:47 +0900

    up: endpoint 변경

--- src/todo-lists/todo-lists.service.ts
+++ src/todo-lists/todo-lists.service.ts
@@ -6,7 +6,7 @@ export class TodoListsService {
 
   async getList() {
     try {
-      const res = await this.httpService.get("http://todo-service.qa/").toPromise();
+      const res = await this.httpService.get("http://todo-service.qa.svc.cluster.local/").toPromise();
       return res.data;
     } catch (error) {
       return error;
