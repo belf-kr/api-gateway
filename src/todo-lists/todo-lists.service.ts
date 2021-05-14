0a41e06e87bc89aea43bf8dece807205cffd265d
Kyungeun <ruddms936@naver.com>
2021-05-14 11:31:13 +0900

    add: 포트 번호 지정

--- src/todo-lists/todo-lists.service.ts
+++ src/todo-lists/todo-lists.service.ts
@@ -6,7 +6,7 @@ export class TodoListsService {
 
   async getList() {
     try {
-      const res = await this.httpService.get("http://todo-service.qa.svc.cluster.local/").toPromise();
+      const res = await this.httpService.get("http://todo-service.qa.svc.cluster.local:3000/").toPromise();
       return res.data;
     } catch (error) {
       return error;
