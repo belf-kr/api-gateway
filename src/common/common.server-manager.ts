c0616cb63174efd13d7adeb86b09f12dd40910ab
Seungup Noh <su.noh@kakao.com>
2021-05-21 23:40:00 +0900

    update 서버 정보를 가져올 수 있는 클래스 작성

--- src/common/common.server-manager.ts
+++ src/common/common.server-manager.ts
@@ -1,48 +1,75 @@
-import { version } from "../../package.json";
-import { HttpStatus } from "@nestjs/common";
+import { HttpService, HttpStatus } from "@nestjs/common";
 
 /**
- * @class CServerManager
+ * @class CServiceInfo
  * @author Seungup
- * @abstract 공통 서버 메니저 클래스
+ * @abstract 공통 서비스 정보 클래스
  * @argument void
  */
-export class CServerManager {
-  private static instance: CServerManager;
-  servers: ServerInfo[] = [];
+export class CServiceInfo {
+  private static instance: CServiceInfo;
+  private httpService: HttpService;
+  private stages: string;
 
-  constructor() {
+  public MOCK_SERVICE: ServiceInfo;
+  public PRODUCT_SERVICE: ServiceInfo;
+
+  private constructor() {
+    this.httpService = new HttpService();
+    this.stages = process.env.STAGES;
     const defaultVersion = "unknown";
 
-    const defaultPing = HttpStatus.FORBIDDEN;
+    const defaultPing = HttpStatus.INTERNAL_SERVER_ERROR;
 
-    this.servers.push({
-      name: "mock-server",
-      status: defaultPing,
+    this.MOCK_SERVICE = {
+      name: "mock-service",
+      httpStatus: defaultPing,
       version: defaultVersion,
-    });
+      port: 3000,
+    };
 
-    this.servers.push({
-      name: "product-server",
-      status: defaultPing,
+    this.PRODUCT_SERVICE = {
+      name: "todo-service",
+      httpStatus: defaultPing,
       version: defaultVersion,
-    });
-
-    this.servers.push({
-      name: "api-gateway",
-      status: HttpStatus.OK,
-      version: version,
-    });
+      port: 3000,
+    };
+    this.init();
   }
 
-  static getInstance() {
-    if (!CServerManager.instance) {
-      CServerManager.instance = new CServerManager();
+  public static getInstance() {
+    if (!CServiceInfo.instance) {
+      CServiceInfo.instance = new CServiceInfo();
     }
-    return CServerManager.instance;
+    return CServiceInfo.instance;
   }
 
-  update() {
-    CServerManager.instance = new CServerManager();
+  public async update() {
+    await this.updateServiceInfo(this.MOCK_SERVICE);
+    await this.updateServiceInfo(this.PRODUCT_SERVICE);
+  }
+
+  private init() {
+    if (process.env.STAGES == "LOCAL") {
+      this.MOCK_SERVICE.port = 3001;
+      this.PRODUCT_SERVICE.port = 3002;
+    }
+  }
+
+  private async updateServiceInfo(service: ServiceInfo) {
+    let SERVICE_URL = `http://${service.name}.${this.stages}.svc.cluster.local:${service.port}`;
+    if (this.stages == "LOCAL") {
+      SERVICE_URL = `http://localhost:${service.port}`;
+    }
+    try {
+      let res = await this.httpService.get(`${SERVICE_URL}/ping`).toPromise();
+      service.httpStatus = res.status;
+
+      res = await this.httpService.get(`${SERVICE_URL}/version`).toPromise();
+      service.version = res.data;
+    } catch (error) {
+      service.httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+      service.error_code = error.code;
+    }
   }
 }
