a0211e1eb767b83167ea986bdf1b2ce3f34180c0
Seungup Noh <su.noh@kakao.com>
2021-05-22 15:49:56 +0900

    update 미들웨어 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 import { AppController } from "./app.controller";
 
 import { TodoController } from "./todo/todo.controller";
@@ -7,10 +7,15 @@ import { MockService } from "./mock/mock.service";
 import { MockModule } from "./mock/mock.module";
 import { TodoModule } from "./todo/todo.module";
 import { TodoService } from "./todo/todo.service";
+import { LoginRequire } from "./middleware/login.middleware";
 
 @Module({
   imports: [MockModule, TodoModule],
   controllers: [AppController, TodoController, MockController],
   providers: [TodoService, MockService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoginRequire).forRoutes({ path: "/*", method: RequestMethod.ALL });
+  }
+}
