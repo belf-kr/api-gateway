8a2113ff334f111d92c3b4b8b6a0c9485d58874c
Seungup Noh <su.noh@kakao.com>
2021-05-22 15:49:25 +0900

    update 사용하지 않는 미들웨어 초기화

--- src/middleware/login.middleware.ts
+++ src/middleware/login.middleware.ts
@@ -1,17 +1,8 @@
-import { HttpException, HttpService, HttpStatus, Injectable, NestMiddleware } from "@nestjs/common";
+import { Injectable, NestMiddleware } from "@nestjs/common";
 
 @Injectable()
 export class LoginRequire implements NestMiddleware {
   async use(request: Request, response: Response, next: () => void): Promise<any> {
-    const authorization = request.headers["Authorization"];
-    if (authorization && process.env.STAGES) {
-      const http = new HttpService();
-      // TODO 로그인 유효성 검사 로직이 추가되어야함.
-      const res = await http.get(`http://www.google.com/`).toPromise();
-      console.log(res.data);
-      next();
-    } else {
-      throw new HttpException("Forbidden", HttpStatus.FORBIDDEN);
-    }
+    next();
   }
 }
