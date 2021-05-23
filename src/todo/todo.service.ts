fec43562cfd4204b84976d1af14c37d1ef6cb07a
Seungup Noh <su.noh@kakao.com>
2021-05-23 10:13:32 +0900

    update 사용하지 않는 임포트 구문 삭제

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { IGlasses, IServiceInfo, ITodos } from "src/common/common.interface";
+import { IServiceInfo } from "src/common/common.interface";
 import { CServiceInfo } from "src/common/common.service-manager";
 
 @Injectable()
