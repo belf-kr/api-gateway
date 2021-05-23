1535fc3d901a22715bed00e3d7aeee8ae565ad03
Seungup <53374248+Seungup@users.noreply.github.com>
2021-05-23 10:15:36 +0900

    사용하지 않는 파일 삭제 및 용도에 맞게 새로 적용된 코드 등 (#16)
    * add : 공통 인터페이스 추가
    
    상태, 버전, 이름의 타입을 가진 서버 정보를 인터페이스로 추가함.
    
    * add : 유저 정보를 가져올 수 있는 커스텀 데코레이터 추가
    
    @Get()
    getUser(@User() user :CUser) {
    ...
    }
    
    @Get()
    getUser(@User("id") userId: string)
    {
    ...
    }
    
    * add : LoginRequire 미들웨어 추가
    
    * add : HTTP 에러 핸들링 클래스 추가
    
    * add : 로깅을 위한 커스텀 로깅 추가
    
    * UP : HTTP 에러 헨들링 로직 추가
    
    * up : 디펜던시 업데이트
    
    * add : 공통 서버 메니저 클래스 추가
    
    * up :
    "/" 변경
    "/ping", "/status" 추가
    
    * del 불필요하 파일 삭제
    
    * update 인터페이스 변경
    
    * update 인터페이스 에러코드를 숫자 형태로 변경
    
    * update 서버 정보를 가져올 수 있는 클래스 작성
    
    * update 기본세팅
    
    * update 10초에 한번 서버의 정보를 가져올 수 있도록 구현
    
    * del 불필요한 파일 삭제
    
    * del 불필요한 파일 삭제
    
    * update mock, todo 서비스에 사용될 공통 인터페이스 추가
    
    * add 공통 전역변수 파일 추가
    
    * add 공통 메시지 추가
    
    * add 공통 메니저 추가
    
    * update 공통 서버 메니저 -> 서비스 정보 메니저로 이름 변경 및 기능 업데이트
    
    * del 구버전 파일 삭제
    
    * update 메인 함수 최적화
    
    * add mock, todo 컨트롤러 추가
    
    * update
    
    * add 변경된 파일 이름 적용
    
    * update | add 인터페이스 용도별로 나눔
    
    * update 네이밍 컨벤션
    
    * update 사용하지 않는 미들웨어 초기화
    
    * update 네이밍 컨벤션
    
    * update 미들웨어 적용
    
    * update 사용하지 않는 임포트 구문 삭제
    
    closes #11
    
    * update 변수 자동 주입 위치 변경
    
    * update 사용하지 않는 파라미터 삭제
    
    * update 불필요한 인터페이스 함수 삭제
    
    * update 오타수정
    
    * update | add | del 인터페이스 명 변경
    
    * update 아직 구현체가 없으니 불필요한 코드 삭제
    
    * update 사용하지 않는 임포트 구문 삭제
    
    Co-authored-by: Seungup Noh <su.noh@kakao.com>

--- src/common/common.define.ts
+++ src/common/common.define.ts
@@ -3,4 +3,9 @@
  */
 
 export const APP_PORT = 3000; // default : 3000
+// 변수 자동 주입
+if (!process.env.STAGES) {
+  process.env.STAGES = "LOCAL";
+}
+
 export const SERVICE_UPDATE_TIMING = 1000 * 1; // default : 1000 * 1
