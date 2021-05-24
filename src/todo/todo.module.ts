d1e60186a81c1b67cebade03f7b3c22e6eff0d06
Kyungeun <ruddms936@naver.com>
2021-05-25 00:23:37 +0900

    add: todo-service 동작 확인 코드 추가

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { TodoService } from "./todo.service";
+import { TestModule } from "./test/test.module";
 
 @Module({
   providers: [TodoService],
+  imports: [TestModule],
 })
 export class TodoModule {}
