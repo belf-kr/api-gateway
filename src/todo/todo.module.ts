05c19fe26b74a40981dcce76efe97cf617005a28
Kyungeun <ruddms936@naver.com>
2021-05-25 01:14:17 +0900

    move: 디렉터리 내부로 모듈 이동

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 
+import { TodoController } from "./todo.controller";
 import { TodoService } from "./todo.service";
 
 import { TestModule } from "./test/test.module";
 
 @Module({
   imports: [TestModule],
+  controllers: [TodoController],
   providers: [TodoService],
 })
 export class TodoModule {}
