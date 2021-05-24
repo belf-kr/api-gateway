2143be6a67e8ce285d1a4c5be656a6eda2819dae
Kyungeun <ruddms936@naver.com>
2021-05-25 01:13:33 +0900

    move: 디렉터리 내부로 모듈 이동

--- src/mock/mock.module.ts
+++ src/mock/mock.module.ts
@@ -1,4 +1,10 @@
 import { Module } from "@nestjs/common";
 
-@Module({})
+import { MockController } from "./mock.controller";
+import { MockService } from "./mock.service";
+
+@Module({
+  controllers: [MockController],
+  providers: [MockService],
+})
 export class MockModule {}
