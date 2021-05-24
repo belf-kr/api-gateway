a5d7769e30b37b3c7e075fc4b3408eb61683e76e
Kyungeun <ruddms936@naver.com>
2021-05-25 01:14:17 +0900

    move: 디렉터리 내부로 모듈 이동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,17 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+
 import { AppController } from "./app.controller";
 
-import { TodoController } from "./todo/todo.controller";
-import { MockController } from "./mock/mock.controller";
-import { MockService } from "./mock/mock.service";
 import { MockModule } from "./mock/mock.module";
+
 import { TodoModule } from "./todo/todo.module";
-import { TodoService } from "./todo/todo.service";
+
 import { LoginRequire } from "./middleware/login.middleware";
 
 @Module({
   imports: [MockModule, TodoModule],
-  controllers: [AppController, TodoController, MockController],
-  providers: [TodoService, MockService],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
