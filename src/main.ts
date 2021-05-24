3165a5adec547cd9e33d224f9b90ebe4ef753348
Kyungeun <ruddms936@naver.com>
2021-05-24 23:54:17 +0900

    fix: 웹 서버 실행은 되도록 수정

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-import { CCommonManager } from "./common/common.manager";
-import { HttpExceptionFilter } from "./filters/http-exception.filter";
+// import { CCommonManager } from "./common/common.manager";
+// import { HttpExceptionFilter } from "./filters/http-exception.filter";
 import { APP_PORT } from "./common/common.define";
 
 import { version } from "../package.json";
 
 async function bootstrap() {
-  if (await CCommonManager.initNestJS()) {
-    const app = await NestFactory.create(AppModule);
-    app.useGlobalFilters(new HttpExceptionFilter());
-    await app.listen(APP_PORT);
-  }
+  // if (await CCommonManager.initNestJS()) {
+  //   const app = await NestFactory.create(AppModule);
+  //   app.useGlobalFilters(new HttpExceptionFilter());
+  //   await app.listen(APP_PORT);
+  // }
+  const app = await NestFactory.create(AppModule);
+  await app.listen(APP_PORT);
 }
 
 console.log(`version: ${version}`);
