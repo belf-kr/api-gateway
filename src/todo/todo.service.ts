c9951e978c50afb52804089f55998e3b8dd26d05
Kyungeun <ruddms936@naver.com>
2021-05-29 15:21:57 +0900

    add: 기본 적인 api lib 작성

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from "@nestjs/common";
+import { HttpService, Injectable } from "@nestjs/common";
+
+import * as todo from "../lib/todo";
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  private todoApiClient: todo.ApiClient;
+
+  constructor(private httpService: HttpService) {
+    this.todoApiClient = new todo.ApiClient(httpService);
+  }
+}
