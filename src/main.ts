49528ddcc01fac54811a7fba6905e1db6b8e6bb7
Kyungeun <ruddms936@naver.com>
2021-05-29 14:25:05 +0900

    add: 기본적인 nestjs 디렉구조 복구

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,12 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
 
-// import { CCommonManager } from "./common/common.manager";
-// import { HttpExceptionFilter } from "./filters/http-exception.filter";
-import { APP_PORT } from "./common/common.define";
+import { AppModule } from "./app.module";
 
 import { version } from "../package.json";
 
 console.log(`version: ${version}`);
 
 async function bootstrap() {
-  // if (await CCommonManager.initNestJS()) {
-  //   const app = await NestFactory.create(AppModule);
-  //   app.useGlobalFilters(new HttpExceptionFilter());
-  //   await app.listen(APP_PORT);
-  // }
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
