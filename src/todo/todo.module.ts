f75375bcdf350a80023a75c32b9ca5937f2f7f79
Kyungeun <ruddms936@naver.com>
2021-05-29 14:52:39 +0900

    add: httpModule

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,9 +1,10 @@
-import { Module } from "@nestjs/common";
+import { HttpModule, Module } from "@nestjs/common";
 
 import { TodoController } from "./todo.controller";
 import { TodoService } from "./todo.service";
 
 @Module({
+  imports: [HttpModule],
   controllers: [TodoController],
   providers: [TodoService],
 })
