8dc0b7db0e577d2bf12af7b25b76b5a0aa4a247d
Kyungeun <ruddms936@naver.com>
2021-05-29 15:21:57 +0900

    add: 기본 적인 api lib 작성

--- src/lib/todo/api.ts
+++ src/lib/todo/api.ts
@@ -1,1 +1,30 @@
-export class ApiClient {}
+import { HttpService } from "@nestjs/common";
+
+export class ApiClient {
+  private url;
+
+  constructor(private httpService: HttpService) {
+    // http://todo-service.qa.svc.cluster.local:3000/
+    const svcName = "todo-service";
+    const stages = process.env.STAGES;
+    this.url = `http://${svcName}.${stages}.svc.cluster.local:3000`;
+  }
+
+  async getVersion() {
+    try {
+      const res = await this.httpService.get(`${this.url}/version`).toPromise();
+      return res.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getEnv() {
+    try {
+      const res = await this.httpService.get(`${this.url}/env`).toPromise();
+      return res.data;
+    } catch (error) {
+      return error;
+    }
+  }
+}
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 
 @Controller("todo")
-export class TodoController {}
+export class TodoController {
+  @Get()
+  getTest1() {
+    return "todo";
+  }
+}
