d77fe1898a533d23a4356f1d8a92a29c3952c11b
Kyungeun <ruddms936@naver.com>
2021-05-30 18:03:51 +0900

    add: mock 연결

--- src/mock/mock.controller.ts
+++ src/mock/mock.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, HttpStatus, Res } from "@nestjs/common";
+import { Response } from "express";
+
+import { MockService } from "./mock.service";
 
 @Controller("mock")
-export class MockController {}
+export class MockController {
+  private readonly appService;
+
+  constructor(appService: MockService) {
+    this.appService = appService;
+  }
+
+  @Get()
+  async getServiceName(): Promise<string> {
+    const result = await this.appService.getServiceName();
+    return result;
+  }
+
+  @Get("ping")
+  getPing(@Res() res: Response): void {
+    res.sendStatus(HttpStatus.OK);
+  }
+
+  @Get("version")
+  async getVersion(): Promise<string> {
+    const result = await this.appService.getVersion();
+    return result;
+  }
+
+  @Get("env")
+  async getEnv(): Promise<NodeJS.ProcessEnv> {
+    const result = await this.appService.getEnv();
+    return result;
+  }
+}
--- src/mock/mock.service.ts
+++ src/mock/mock.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from "@nestjs/common";
 
+import { MockApiClient } from "./lib/api";
+
 @Injectable()
-export class MockService {}
+export class MockService {
+  private mockApiClient: MockApiClient;
+
+  constructor(mockApiClient: MockApiClient) {
+    this.mockApiClient = mockApiClient;
+  }
+
+  async getServiceName() {
+    try {
+      const result = await this.mockApiClient.getServiceName();
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
+  async getPing() {
+    try {
+      const result = await this.mockApiClient.getPing();
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
+  async getVersion() {
+    try {
+      const result = await this.mockApiClient.getVersion();
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
+  async getEnv() {
+    try {
+      const result = await this.mockApiClient.getEnv();
+      return result;
+    } catch (error) {
+      return error;
+    }
+  }
+}
