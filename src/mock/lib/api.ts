5232e4863f2bb2d7504d0acd890dd5d5c852e9ef
Kyungeun <ruddms936@naver.com>
2021-05-30 18:03:51 +0900

    add: mock 연결

---
+++ src/mock/lib/api.ts
@@ -0,0 +1,43 @@
+import { HttpService, Injectable } from "@nestjs/common";
+
+@Injectable()
+export class MockApiClient {
+  private httpService: HttpService;
+
+  constructor(httpService: HttpService) {
+    this.httpService = httpService;
+  }
+
+  async getServiceName() {
+    try {
+      const res = await this.httpService.get("/").toPromise();
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getPing() {
+    try {
+      const res = await this.httpService.get("/ping").toPromise();
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getVersion() {
+    try {
+      const res = await this.httpService.get("/version").toPromise();
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getEnv() {
+    try {
+      const res = await this.httpService.get("/env").toPromise();
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
--- src/mock/mock.module.ts
+++ src/mock/mock.module.ts
@@ -1,10 +1,20 @@
-import { Module } from "@nestjs/common";
+import { HttpModule, Module } from "@nestjs/common";
 
 import { MockController } from "./mock.controller";
 import { MockService } from "./mock.service";
 
+import { MockApiClient } from "./lib/api";
+
+// FIXME: (parkgang) DI 받을 수 있도록 수정되어야 함
+import { K8sServiceDNS } from "../common/lib/service";
+
 @Module({
+  imports: [
+    HttpModule.register({
+      baseURL: K8sServiceDNS("mock-service", 3000),
+    }),
+  ],
   controllers: [MockController],
-  providers: [MockService],
+  providers: [MockService, MockApiClient],
 })
 export class MockModule {}
