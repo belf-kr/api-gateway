31f855bd30bf511a07ebbf045f47164484a013a7
박경은 <ruddms936@naver.com>
2021-06-04 03:56:59 +0900

    up: 토큰 검사하는 가짜 미들웨어 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,15 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
-import { OauthMiddleware } from "./middleware/oauth.middleware";
-
 import { TodoModule } from "./todo/todo.module";
 import { MockModule } from "./mock/mock.module";
+import { MiddlewareModule } from "./middleware/middleware.module";
 
 @Module({
-  imports: [TodoModule, MockModule],
+  imports: [TodoModule, MockModule, MiddlewareModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(OauthMiddleware).forRoutes({ path: "/*", method: RequestMethod.ALL });
-  }
-}
+export class AppModule {}
--- src/middleware/oauth.middleware.ts
+++ src/middleware/oauth.middleware.ts
@@ -1,10 +1,27 @@
-import { Injectable, NestMiddleware } from "@nestjs/common";
+import { HttpService, Injectable, NestMiddleware } from "@nestjs/common";
 import { NextFunction } from "express";
 
 @Injectable()
 export class OauthMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+  private readonly httpService: HttpService;
+
+  constructor(httpService: HttpService) {
+    this.httpService = httpService;
+  }
+
+  async tokenValid() {
+    try {
+      const res = await this.httpService.get("/token/valid").toPromise();
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async use(req: Request, res: Response, next: NextFunction) {
     console.log(`OauthMiddleware`);
+    const result = await this.tokenValid();
+    console.log(result);
     next();
   }
 }
