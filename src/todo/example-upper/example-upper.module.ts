0c1b0661bd3f16c9d7570b528d3a17c3bfce0f73
박경은 <ruddms936@naver.com>
2021-06-05 21:50:38 +0900

    style: 순서 변경

--- src/todo/example-upper/example-upper.module.ts
+++ src/todo/example-upper/example-upper.module.ts
@@ -1,13 +1,13 @@
 import { forwardRef, Module } from "@nestjs/common";
 
-import { ExampleUpperService } from "./example-upper.service";
 import { ExampleUpperController } from "./example-upper.controller";
+import { ExampleUpperService } from "./example-upper.service";
 
 import { TodoModule } from "../todo.module";
 
 @Module({
   imports: [forwardRef(() => TodoModule)],
-  providers: [ExampleUpperService],
   controllers: [ExampleUpperController],
+  providers: [ExampleUpperService],
 })
 export class ExampleUpperModule {}
