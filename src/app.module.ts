206ee35c783228c3f5609bb52e81c91019973d6c
JP3Pe <rlaworhkd430430@gmail.com>
2021-06-08 18:05:26 +0900

    add: Npm(NodeJS) 설정 주입을 todo와 동일하게 수정
    closes #21

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,31 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import AppConfig from "./config/app.config";
 
 import { TodoModule } from "./todo/todo.module";
 import { MockModule } from "./mock/mock.module";
 import { MiddlewareModule } from "./middleware/middleware.module";
 
 @Module({
-  imports: [TodoModule, MockModule, MiddlewareModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: false,
+      load: [AppConfig],
+      // prod 환경의 환경변수는 모두 k8s가 컨트롤
+      ignoreEnvFile: process.env.NODE_ENV === "production",
+      envFilePath: process.env.NODE_ENV === "development" ? ".env.dev" : "",
+      validationSchema: Joi.object({
+        SERVER_PORT: Joi.number().default(3000),
+      }),
+    }),
+    TodoModule,
+    MockModule,
+    MiddlewareModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
