36b41f3c64bf3918b6b1eac6ea22e4076895384e
JP3Pe <rlaworhkd430430@gmail.com>
2021-06-09 13:12:04 +0900

    add: mock 서비스 listen port .env.dev 사용한 주입 기능 추가
    기존 todo Typeorm 코드, NestJS HTTP module 메뉴얼 참고
    
    closes #22

--- src/mock/mock.module.ts
+++ src/mock/mock.module.ts
@@ -1,17 +1,32 @@
 import { HttpModule, Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
+import * as Joi from "joi";
 
 import { MockController } from "./mock.controller";
 import { MockService } from "./mock.service";
 
 import { MockApiClient } from "./lib/api";
 
-// FIXME: (parkgang) DI 받을 수 있도록 수정되어야 함
-import { K8sServiceDNS } from "../common/lib/service";
+import { K8sServiceDNS } from "src/common/lib/service";
 
 @Module({
   imports: [
-    HttpModule.register({
-      baseURL: K8sServiceDNS("mock-service", 3000),
+    ConfigModule.forRoot({
+      isGlobal: false,
+      // prod 환경의 환경변수는 모두 k8s가 컨트롤
+      ignoreEnvFile: process.env.NODE_ENV === "production",
+      envFilePath: process.env.NODE_ENV === "development" ? ".env.dev" : "",
+      validationSchema: Joi.object({
+        MOCK_SERVER_PORT: Joi.number().default(3000),
+      }),
+    }),
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        baseURL: K8sServiceDNS("mock-service", configService.get("MOCK_SERVER_PORT")),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [MockController],
