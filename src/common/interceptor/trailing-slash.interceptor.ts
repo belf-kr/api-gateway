1151710759f90cc9b9b05cec08e6dc28a56fc524
JP3Pe <rlaworhkd430430@gmail.com>
2021-07-01 15:54:01 +0900

    add: 인터셉터 관련 주석 추가 #37 이슈에 대한 설명을 약간 추가
    closes #39

--- src/common/interceptor/trailing-slash.interceptor.ts
+++ src/common/interceptor/trailing-slash.interceptor.ts
@@ -2,6 +2,7 @@ import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nes
 import { Observable } from "rxjs";
 
 @Injectable()
+// REST API 같은 HTTP API를 보다 자연스럽게 제공하기 위한 일종의 방어코드
 // API 요청이 후행 슬래시로 끝나는 경우, 사용자의 요청을 올바른 후행 슬래시가 없는 URL로 라다이렉트
 export class TrailingSlashInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
