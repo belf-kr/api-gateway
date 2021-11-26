9d7e76248104620695cc9609ebdf73ee1d226a66
JP3Pe <rlaworhkd430430@gmail.com>
2021-11-26 12:24:42 +0900

    add: API Controller내 ping용 로그 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,6 +17,7 @@ export class AppController {
 
   @Get("ping")
   getPing(): void {
+    console.log(`Begin controller: ${new Date().toUTCString()}, and ${new Date().getUTCMilliseconds()}ms`);
     return;
   }
 
