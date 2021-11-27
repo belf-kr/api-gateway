f532c49610ae0974e2256b79105cc8a1dc67c8a2
JP3Pe <45834750+JP3Pe@users.noreply.github.com>
2021-11-27 13:41:07 +0900

    work-todo 조회시 courseId 가져올 수 있게 타입코드 수정 (#93)

--- src/common/type/work-todo-get.interface.ts
+++ src/common/type/work-todo-get.interface.ts
@@ -3,4 +3,6 @@ import { WorkTodoType } from "./work-todo.type";
 export interface WorkTodoGetInterface extends WorkTodoType {
   courseTitle: string;
   color: string;
+  repeatedDaysOfTheWeek: number[];
+  courseId: number;
 }
